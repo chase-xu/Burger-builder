42611783a34d6f2def9508f1515c4308aae6b1f9
Chase <pengxu1993@gmail.com>
2020-10-01 22:47:26 -0700

    Updates

---
+++ precache-manifest.1eb7414cb710af78f807381ad80e6a82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90181583945a6c00780c8e66e6f0669e",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "f2b66d5ae44ef993a113",
+    "url": "/burger-builder/static/css/main.e7b1ac24.chunk.css"
+  },
+  {
+    "revision": "2b5a8175c262144d015a",
+    "url": "/burger-builder/static/js/2.57d9a754.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burger-builder/static/js/2.57d9a754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2b66d5ae44ef993a113",
+    "url": "/burger-builder/static/js/main.60c0c029.chunk.js"
+  },
+  {
+    "revision": "3743a9caf0853cc6f96a",
+    "url": "/burger-builder/static/js/runtime-main.f3cd13ce.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
